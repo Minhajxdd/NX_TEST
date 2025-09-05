aca0e783193f71bec378fdd569f3fcc85fac56dd
Mohammed Minhaj <nijulinux@gmail.com>
2025-09-05 23:45:33 +0530

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+import { Routes } from "./api/routes";
+
+class App {
+  public app: Application;
+  public routePrv: Routes = new Routes();
+
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routePrv.routes(this.app);
+  }
+
+  private config(): void {
+    this.app.use((req: Request, res: Response, next: NextFunction) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS,PUT");
+      res.header("Access-Control-Allow-Headers", "*");
+      next();
+    });
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+  }
+}
+
+export default new App().app;
